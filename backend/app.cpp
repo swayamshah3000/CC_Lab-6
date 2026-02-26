ac9b167d44839fe39f77aa68d465e6d971747a3b
Swayam <swayamshah3000@github.com>
2026-02-26 08:27:32 +0000

    Keep backend containers alive for NGINX discovery

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <unistd.h> // for sleep
 using namespace std;
 
 int main() {
     cout << "Served by backend: PES1UG23CS625" << endl;
+    // Keep the container alive for NGINX to find it
+    while (true) {
+        sleep(100); 
+    }
     return 0;
 }
